0e827b9486e31e4511c4035545928801dc615d5d
JosephLee19 <leejos17@gmail.com>
2017-04-08 11:55:20 -0400

    material property stuff

---
+++ TrussAnalysis_Testing/bridge.cpp
@@ -0,0 +1,86 @@
+
+#include <iostream>
+
+//Poisson's ratio (when you compress something, this ratio discribes the thickness increase or decrease)
+
+Structural Steel:
+Density=7860//kg/m^3
+Yield Strength=260//MPa
+Young's Modulus=200//GPa
+
+High Strength Low Carbon Steel:
+Density=7860//kg/m^3
+Yield Strength=350//MPa
+Young's Modulus=200//GPa
+
+Cold-Rolled Steel:
+Density=7920//kg/m^3
+Yield Strength=500//MPa
+Young's Modulus=190//GPa
+
+Cast Iron:
+Density=7300//kg/m^3
+Yield Strength=230//MPa
+Young's Modulus=190//GPa
+
+Aluminum 6061-T6:
+Density=2710//kg/m^3
+Yield Strength=255//MPa
+Young's Modulus=69//GPa
+
+Yellow Brass Cold-Rolled:
+Density=8470//kg/m^3
+Yield Strength=435//MPa
+Young's Modulus=105//GPa
+
+Yellow Brass Annealed:
+Density=8470//kg/m^3
+Yield Strength=105//MPa
+Young's Modulus=105//GPa
+
+Phosphor Bronze Cold-Rolled:
+Density=8860//kg/m^3
+Yield Strength=520//MPa
+Young's Modulus=110//GPa
+
+Magnesium Alloy:
+Density=1800//kg/m^3
+Yield Strength=275//MPa
+Young's Modulus=45//GPa
+
+Titanium Alloy:
+Density=4460//kg/m^3
+Yield Strength=825//MPa
+Young's Modulus=114//GPa
+
+
+
+
+
+/* Armadillo testing stuff
+#include <iostream>
+#include <armadillo>
+
+using namespace std;
+using namespace arma;
+
+int main()
+  {
+mat B;
+B << 0 << 0 << endr
+  << 1 << 0 << endr
+  << 2 << 0 << endr
+  << 3 << 0 << endr
+  << 1 << .75 << endr
+  << 2 << .75 << endr;
+cout<<B<<endl;
+
+//zeros(2*sizeof(rows of points),number of forces)
+//for(point in B){
+
+//}
+//g++ -std=c++11 bridge.cpp -o bridge -larmadillo && ./bridge
+//cout<<bla<<endl;
+  return 0;
+  }
+*/
