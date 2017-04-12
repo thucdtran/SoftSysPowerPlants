487254bb163c6a94173323cad69ed818f3a3fdf6
JosephLee19 <leejos17@gmail.com>
2017-04-12 16:45:02 -0400

    Added pseudo code cost func and updated materials

--- TrussAnalysis_Testing/material_properties.cpp
+++ TrussAnalysis_Testing/material_properties.cpp
@@ -7,23 +7,29 @@ using namespace std;
 
 int main()
 {
-    map<string, vector<int> > material_properties;
+    map<string, vector<double> > material_properties;
     // Inserting data in std::map
-    material_properties["Structural Steel"] = {7860, 260, 200};
-    material_properties["Low Carbon Steel"] = {7860, 350, 200};
-    material_properties["Cold-Rolled Steel"] = {7920, 500, 190};
-    material_properties["Cast Iron"] = {7300, 230, 190};
-    material_properties["Aluminum 6061-T6"] = {2710, 255, 69};
-    material_properties["Yellow Brass Cold-Rolled"] = {8470, 435, 105};
-    material_properties["Yellow Brass Annealed"] = {8470, 105, 105};
-    material_properties["Phosphor Bronze Cold-Rolled"] = {8860, 520, 110};
-    material_properties["Magnesium Alloy"] = {1800, 275, 45};
-    material_properties["Titanium Alloy"] = {4460, 825, 114};
+    //based on HEB 260B I-beam, cross sectional area is 0.0125 m^2
+    //weight per m^2 = density * volume per meter
+    //{Density,Yield Strength,Young's Modulus,Dollars per Meter}
+
+    //material costs are found from a table at http://www.roymech.co.uk/Useful_Tables/Matter/Costs.html
+    //for convenience, the relative costs of the materials will be multiplied by a 'cost factor'
+    double area = 0.0125;//cross sectional area of I-Beam
+    double cost_factor = 50.50;//accounts for beam size and estimate of manufacturing costs
+    material_properties["ASTM A992 Steel"] = {7850*area, 345.0, 200.0,1.51*cost_factor};
+    material_properties["Low Carbon Steel"] = {7860*area, 350.0, 200.0, 1*cost_factor};
+    material_properties["Cold-Rolled Steel"] = {7920*area, 500.0, 190.0,1.51*cost_factor};
+    material_properties["Cast Iron"] = {7300*area, 230.0, 190.0,1.51*cost_factor};
+    material_properties["Aluminum 6061-T6"] = {2710*area, 255.0, 69.0,4.01*cost_factor};
+    material_properties["Yellow Brass Cold-Rolled"] = {8470*area, 435.0, 105.0,10.2*cost_factor};
+    material_properties["Yellow Brass Annealed"] = {8470*area, 105.0, 105.0,10.2*cost_factor};
+    material_properties["Phosphor Bronze Cold-Rolled"] = {8860*area, 520.0, 110.0,12*cost_factor};
+    material_properties["Magnesium Alloy"] = {1800*area, 275.0, 45.0,7.3*cost_factor};
+    material_properties["Titanium Alloy"] = {4460*area, 825.0, 114.0,30.9*cost_factor};
     return 0;
 
 
-    vector<int> mat_prop(3) mat_prop;
-    mat_prop=material_properties.find("Cast Iron");
 
 }
 
